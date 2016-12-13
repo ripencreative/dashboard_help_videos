b556bd4a9902dfb8c64b323e61a03c61557a5900
ripencreative <brianmorris@Brians-MacBook-Pro.local>
2016-12-13 08:49:44 -0800

    Add 2 more Posts videos

---
+++ menu/posts.php
@@ -0,0 +1,45 @@
+<div class="wrap about-wrap">
+    <h1>WordPress Posts Menu Help Videos</h1>
+
+    <div class="about-text">
+        <p>The following videos are instructional for the Dashboard menu. Thanks again to <a href="http://wordpress.tv" target="_blank">wordpress.tv</a> for the videos.</p>
+    </div>
+
+    <div class="allvideos">
+        <h2>Posts</h2>
+        <div class="video">
+            <iframe width="560" height="315" src="https://videopress.com/embed/AlyXRIK7" frameborder="0" allowfullscreen></iframe>
+            <script src="https://videopress.com/videopress-iframe.js"></script>
+        </div>
+        <div class="video">
+            <iframe width="560" height="315" src="https://videopress.com/embed/K7h1Jx6p" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="video">
+            <iframe width="560" height="315" src="https://videopress.com/embed/1UKXOM9q" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="video">
+            <iframe width="560" height="315" src="https://videopress.com/embed/mAQazAM6" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="video">
+            <iframe width="560" height="315" src="https://videopress.com/embed/VN5O01er" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="video">
+            <iframe width="560" height="315" src="https://videopress.com/embed/yPnnnqoF" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="video">
+            <iframe width="560" height="315" src="https://videopress.com/embed/jTgaPXc9" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="video">
+            <iframe width="560" height="315" src="https://videopress.com/embed/RfyxLxGh" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="video">
+            <iframe width="560" height="315" src="https://videopress.com/embed/duPLFt5X" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
+<span style="clear: left;"></span>
+<div id="footer">
+    <hr>
+    <p>This plugin is the creation of Brian Morris from <a href="https://ripencreative.ca" target="_blank">RIPEn Creative</a> and <a href="https://wpsitemaintain.ca" target="_blank">WP Site Maintain</a></p>
+    <p>If you have any questions or concerns with the plugin you can reach our team at <a href="mailto: support@wpsitemaintain.ca">support@wpsitemaintain.ca</a></p>
+</div>
--- readme.txt
+++ readme.txt
@@ -3,11 +3,11 @@ Requires at least: 4.0
 Tested up to: 4.7
 Stable tag: 1.2.0
 
-This plugin adds help videos to the dashboard of WordPress installations
+This plugin adds help videos to the dashboard of WordPress installations. The videos are from wordpress.tv.
 
 == Description ==
 
-This plugin adds help videos to the dashboard of WordPress installations.
+This plugin adds help videos to the dashboard of WordPress installations. The videos are from wordpress.tv.
 
 == Important Notes ==
 
@@ -16,5 +16,5 @@ Activate this at all WordPress websites.
 
 == Changelog ==
 
-= 0.1 =
-* 12/2016 - First version adding a Help menu item
+= 1.0 =
+* 12/2016 - First version of the plugin including instructional videos for Dashboard and Posts.
