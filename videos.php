3fe5c62fed4e5003096f9b11a597e595a7731116
ripencreative <brianmorris@Brians-MacBook-Pro.local>
2016-12-14 17:05:46 -0800

    GitHub Plugin updates via TGM Activation

--- videos.php
+++ videos.php
@@ -6,32 +6,19 @@
     </div>
 
     <div class="allvideos">
-        <h2><?php _e( 'Dashboard' ); ?></h2>
+        <h2>Menu Option</h2>
         <div class="video">
-            <?php _e('<iframe width="560" height="315" src="https://videopress.com/embed/6i3tnkW8" frameborder="0" allowfullscreen></iframe>
-<script src="https://videopress.com/videopress-iframe.js"></script>'); ?>
+            <a href="https://ripen-creative.dev/wp-admin/admin.php?page=dashboard_help_videos"><img src="images/dashboard.jpg" title="Click to view dashboard help videos" /></a>
         </div>
         <div class="video">
-            <?php _e('<iframe width="560" height="315" src="https://videopress.com/embed/XO145v3h" frameborder="0" allowfullscreen></iframe>'); ?>
-        </div>
-    </div>
-    <div class="allvideos">
-        <h2><?php _e( 'Posts' ); ?></h2>
-        <div class="video">
-            <?php _e('<iframe width="560" height="315" src="https://videopress.com/embed/AlyXRIK7" frameborder="0" allowfullscreen></iframe>'); ?>
-        </div>
-        <div class="video">
-            <?php _e('<iframe width="560" height="315" src="https://videopress.com/embed/1UKXOM9q" frameborder="0" allowfullscreen></iframe>'); ?>
-        </div>
-        <div class="video">
-            <?php _e('<iframe width="560" height="315" src="https://videopress.com/embed/mAQazAM6" frameborder="0" allowfullscreen></iframe>'); ?>
+            <a href="https://ripen-creative.dev/wp-admin/admin.php?page=posts_help_videos"><img src="images/posts.jpg" title="Click to view posts help videos" /></a>
         </div>
     </div>
 </div>
 <span style="clear: left;"></span>
 <div id="footer">
-    <?php _e('<hr>' ); ?>
-     <?php _e('<p>This plugin is the creation of Brian Morris from <a href="https://ripencreative.ca" target="_blank">RIPEn Creative</a> and <a href="https://wpsitemaintain.ca" target="_blank">WP Site Maintain</a></p>' ); ?>
-    <?php _e('<p>If you have any questions or concerns with the plugin you can reach our team at <a href="mailto: support@wpsitemaintain.ca">support@wpsitemaintain.ca</a></p>' ); ?>
+    <hr>
+     <p>This plugin is the creation of Brian Morris from <a href="https://ripencreative.ca" target="_blank">RIPEn Creative</a> and <a href="https://wpsitemaintain.ca" target="_blank">WP Site Maintain</a></p>
+    <p>If you have any questions or concerns with the plugin you can reach our team at <a href="mailto: support@wpsitemaintain.ca">support@wpsitemaintain.ca</a></p>
 </div>
 
